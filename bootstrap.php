0daff78656de7572f1c7ddbdd26e48646ef6aa5e
jpereira <josebatistapereira@gmail.com>
2020-08-31 10:31:59 -0300

    Sistema de rotas

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,21 @@
 <?php
 
+use JBP\Framework\Exceptions\HttpException;
+
 require __DIR__ . "/vendor/autoload.php";
 
-new \JBP\Framework\Router();
+$route = new \JBP\Framework\Router();
+
+$route->add('get', '/', function () {
+    return 'estamos na home';
+});
+
+$route->add('GET', '/projects/(\d+)', function ($params) {
+    return 'listando projeto com id ' . $params[1];
+});
+
+try {
+    echo $route->run();
+} catch (HttpException $e) {
+    echo json_encode(['error' => $e->getMessage()]);
+}
--- src/Router.php
+++ src/Router.php
@@ -2,7 +2,46 @@
 
 namespace JBP\Framework;
 
+use JBP\Framework\Exceptions\HttpException;
+
 class Router
 {
+
+    private $routes = [];
+
+    public function add(string $method, string $pattern, $callback)
+    {
+        $method = strtolower($method);
+        $pattern = str_replace('/', '\/', $pattern);
+        $pattern = "/^$pattern$/";
+        $this->routes[$method][$pattern] = $callback;
+    }
+
+    private function getCurrentUrl()
+    {
+        $url = $_SERVER['PATH_INFO'] ?? '/';
+        if (strlen($url) > 1) {
+            $url = rtrim($url, '/');
+        }
+        return $url;
+    }
+
+    public function run()
+    {
+        $url = $this->getCurrentUrl();
+        $method = strtolower($_SERVER['REQUEST_METHOD']);
+
+        if (empty($this->routes[$method])) {
+            throw new HttpException('Page not found', 404);
+        }
+
+        foreach ($this->routes[$method] as $route => $action) {
+            if (preg_match($route, $url, $params)) {
+                return $action($params);
+            }
+        }
+
+        throw new HttpException('Page not found', 404);
+    }
 
 }
