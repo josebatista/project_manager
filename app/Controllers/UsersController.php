d6e4285620cacf7025ab96e12b289b5d45ac8cb7
jpereira <josebatistapereira@gmail.com>
2020-09-07 10:17:55 -0300

    adicionado HttpFoundation ao projeto

--- app/Controllers/UsersController.php
+++ app/Controllers/UsersController.php
@@ -5,14 +5,17 @@ namespace APP\Controllers;
 
 
 use APP\Models\Users;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class UsersController
 {
 
-    public function show($container, $params)
+    public function show($container, Request $request)
     {
+
         $user = new Users($container);
-        $data = $user->get($params[1]);
+        $data = $user->get($request->attributes->get(1));
 
         return 'Olá meu nome é ' . $data['name'];
     }
