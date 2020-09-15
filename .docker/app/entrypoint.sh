687ff68d1d1c92ce131004e1bac3fe22162c52a2
jpereira <josebatistapereira@gmail.com>
2020-09-15 09:05:24 -0300

    alterado entrypoint do container app para executar em bg o watch e build para o vue.js

--- .docker/app/entrypoint.sh
+++ .docker/app/entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 composer install
+# Running Vue.js development build in background.
+npm run watch --prefix ./front &
+
+# Running Vue.js dashboard in background. (NEED EXPOSE PORT 8000 in 'docker-compose' file)
+#vue ui --host 0.0.0.0 &
+
 php-fpm
